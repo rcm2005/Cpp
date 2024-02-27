a25423c6b82cdad0b8e2376f1f5f239286128f85
rafaelmattos <rcm2005@icloud.com>
2024-02-27 18:24:15 -0300

    criando primeiro arquivo, testando a sintaxe

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    cout <<"*********************" <<endl;
+    cout <<"******Bem vindo******" <<endl;
+    cout <<"*********************";
+
+    int numero_secreto = 42;
+
+    cout<< "o numero secreto n�o ." << numero_secreto<< "Não conte para ninguém"<< endl;
+}
